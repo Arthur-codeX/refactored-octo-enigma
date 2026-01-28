7802663427029c1744327b2c01a7a3f69472e5b1
ArthurcodeX <daniel.karanja239@gmail.com>
2026-01-28 16:46:33 +0300

    updated main

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Daniel
 
 Chapter 1 page 1
-#Mwallow
+# Mwallow
 Chapter 1 page 2
-#Joshua Odero
+# Joshua Odero
 Chapter 2 page 1
--- src/App/Chapter2/Page2.jsx
+++ src/App/Chapter2/Page2.jsx
@@ -1,7 +1,9 @@
 function Page() {
   return (
     <div>
-      <h1>Page 2 comming soon</h1>
+      <h1>Daniel made changes to this file</h1>
+      <h1>Daniel made a mistake he dinot create a branch</h1>
+      <h1>Commit history is wrong.</h1>
     </div>
   );
 }
