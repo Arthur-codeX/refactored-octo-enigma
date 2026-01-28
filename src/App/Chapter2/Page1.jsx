f7275abe640db9d6041ef6269fe1fede47427b5b
ArthurcodeX <daniel.karanja239@gmail.com>
2026-01-28 16:49:33 +0300

    updated change

--- src/App/Chapter2/Page1.jsx
+++ src/App/Chapter2/Page1.jsx
@@ -1,9 +1,7 @@
 function Page() {
   return (
     <div>
-      <h1>Daniel made changes to this file</h1>
-      <h1>Daniel made a mistake he dinot create a branch</h1>
-      <h1>Commit history is wrong.</h1>{" "}
+      <h1>Page 1 comming soon</h1>
     </div>
   );
 }
